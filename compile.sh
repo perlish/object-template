dd86e3c811139bc337c23fc3001bf44a43ccc30b
Seyed Hadi Hosseini <im.perlish@gmail.com>
2011-07-07 15:32:18 +0430

    fix compile script

--- compile.sh
+++ compile.sh
@@ -12,6 +12,7 @@ ld -shared -soname libprint.so.1 -o libprint.so.1.0 -lc libprint.o
 # link shared object
 ln -sf libprint.so.1.0 libprint.so.1
 ln -sf libprint.so.1 libprint.so
+cp libprint.so.1.0 /usr/lib/
 
 # make link for library
 export LD_LIBRARY_PATH=.:$LD_LIBRARY_PATH
@@ -19,3 +20,4 @@ export LD_LIBRARY_PATH=.:$LD_LIBRARY_PATH
 
 gcc -o example example.c -L. -lprint
 # compile test program for external call
+
