8fb62fffc9fdd6d148665fdd94c26e3b0e9de247
Seyed Hadi Hosseini <im.perlish@gmail.com>
2011-07-07 15:44:24 +0430

    finish compile script

--- compile.sh
+++ compile.sh
@@ -1,23 +1,27 @@
+echo "cleanup \n";
+#Cleanup
+rm example
+rm libprint.o
 rm libprint.so
 rm libprint.so.1
-
-rm libprint.o
-rm example
-#clean up
-
-gcc -fPIC -c libprint.c
+rm libprint.so.1.0
+echo "compile library";
 # compile to position indipendent code
 
+gcc -fPIC -c libprint.c
+echo "link library to shared object";
+# link shared objects
+
 ld -shared -soname libprint.so.1 -o libprint.so.1.0 -lc libprint.o
-# link shared object
+echo "expand library\n";
+# make link and copy to library path
 ln -sf libprint.so.1.0 libprint.so.1
 ln -sf libprint.so.1 libprint.so
 cp libprint.so.1.0 /usr/lib/libprint.so
-
-# make link for library
+# export library path
 export LD_LIBRARY_PATH=.:$LD_LIBRARY_PATH
 
-
-gcc -o example example.c -L. -lprint
+echo "compile example program \n";
 # compile test program for external call
+gcc -o example example.c -L. -lprint
 
